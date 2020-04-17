ae4d2e7513457a4d55cb3056aa77867469e44698
Ben Coleman <benc.uk@gmail.com>
2020-04-17 17:38:19 +0100

    Some kubernetes some frontend stuff

--- scripts/restart-dapr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl rollout restart -n dapr-system deploy/dapr-operator
-kubectl rollout restart -n dapr-system deploy/dapr-placement
-kubectl rollout restart -n dapr-system deploy/dapr-sentry
-kubectl rollout restart -n dapr-system deploy/dapr-sidecar-injector
