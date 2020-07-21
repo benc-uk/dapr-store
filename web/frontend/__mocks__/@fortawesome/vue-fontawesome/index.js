d077849ce564337d0bbd8af64bd8c8a95e017aee
Ben Coleman <benc.uk@gmail.com>
2020-07-21 09:20:45 +0100

    tests: new mocks and removed unused mocks

--- web/frontend/__mocks__/@fortawesome/vue-fontawesome/index.js
+++
@@ -1,8 +0,0 @@
-// Stubs out FontAwesome icons and prevents SVGs leaking into tests
-import Vue from 'vue'
-
-export default Vue.component('fa', {
-  render(createElement) {
-    return createElement('i')
-  }
-})
---
+++ web/frontend/src/services/__mocks__/auth.js
@@ -0,0 +1,14 @@
+export default {
+  user() {
+    return {
+      accountIdentifier: 'e11d4d0c-1c70-430d-a644-aed03a60e059',
+      homeAccountIdentifier: '',
+      userName: 'demo@example.net',
+      name: 'Demo User',
+      idToken: null,
+      idTokenClaims: null,
+      sid: '',
+      environment: ''
+    }
+  }
+}
