04d1f7024fbdf7a1a32fdf0e1dded142cad88e4c
Ben Coleman <benc.uk@gmail.com>
2021-04-12 08:58:52 +0100

    :truck: Moving, tidy & cleanup

--- recycle-bin/OLD/ingress/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-helm upgrade --install api-gateway stable/nginx-ingress -f $DIR/chart-values.yaml
-kubectl apply -f $DIR/rules.yaml
--- recycle-bin/api-gateway.Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM nginx:alpine
-
-# Copy in custom reverse proxy config
-RUN rm /etc/nginx/conf.d/*
-COPY build/api-gateway.conf /etc/nginx/conf.d/api-gateway.conf
